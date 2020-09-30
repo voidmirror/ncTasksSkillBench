33cc3bb7a2c7229cd99bf28f1b4c08c715996f38
voidmirror <49756045+voidmirror@users.noreply.github.com>
2020-09-30 10:28:49 +0300

    init task 1

---
+++ ncTask2/src/ru/skillbench/tasks/text/ContactCardImpl.java
@@ -0,0 +1,54 @@
+package ru.skillbench.tasks.text;
+
+import java.time.Period;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class ContactCardImpl implements ContactCard {
+
+
+    @Override
+    public ContactCard getInstance(Scanner scanner) {
+        return null;
+    }
+
+    @Override
+    public ContactCard getInstance(String data) {
+        return null;
+    }
+
+    @Override
+    public String getFullName() {
+        return null;
+    }
+
+    @Override
+    public String getOrganization() {
+        return null;
+    }
+
+    @Override
+    public boolean isWoman() {
+        return false;
+    }
+
+    @Override
+    public Calendar getBirthday() {
+        return null;
+    }
+
+    @Override
+    public Period getAge() {
+        return null;
+    }
+
+    @Override
+    public int getAgeYears() {
+        return 0;
+    }
+
+    @Override
+    public String getPhone(String type) {
+        return null;
+    }
+}
